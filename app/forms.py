7e9b977b5705abb617d4cdc61be5dca956a7ae90
Javy47 <javaughnmiller@gmail.com>
2020-10-08 12:02:28 -0400

    Created a registration form and implimented teh ability to migrate database easily when needed

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -9,3 +10,21 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
+class RegistrationFrom(FlaskForm):
+    username = StringField('Username',validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Register')
+
+    #When you add any methods that match the pattern validate_<field_name>, WTForms
+    #takes those as custom validators and invokes them in addition to the stock validators
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please Enter a different username')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email is not None:
+            raise ValidationError('Please use a different address')
